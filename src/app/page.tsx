9f44c6b076fde1a35c52e15ab9bb35e5b239b313
Pablo Romano <pabloezeromano@gmail.com>
2025-08-21 11:50:17 -0300

    Extend Red Flags and Safe Practices with 6 additional community-driven items (items 4-9) in both English and Spanish
    Signed-off-by: Pablo Romano <pabloezeromano@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,6 +94,90 @@ export default function Home() {
                 {t('home.redFlags.item3Desc')}
               </p>
             </div>
+
+            <div className="bg-red-50 rounded-lg p-6 border border-red-200">
+              <div className="text-red-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.redFlags.item4')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.redFlags.item4Desc')}
+              </p>
+            </div>
+
+            <div className="bg-red-50 rounded-lg p-6 border border-red-200">
+              <div className="text-red-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.redFlags.item5')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.redFlags.item5Desc')}
+              </p>
+            </div>
+
+            <div className="bg-red-50 rounded-lg p-6 border border-red-200">
+              <div className="text-red-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.redFlags.item6')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.redFlags.item6Desc')}
+              </p>
+            </div>
+
+            <div className="bg-red-50 rounded-lg p-6 border border-red-200">
+              <div className="text-red-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.redFlags.item7')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.redFlags.item7Desc')}
+              </p>
+            </div>
+
+            <div className="bg-red-50 rounded-lg p-6 border border-red-200">
+              <div className="text-red-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.redFlags.item8')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.redFlags.item8Desc')}
+              </p>
+            </div>
+
+            <div className="bg-red-50 rounded-lg p-6 border border-red-200">
+              <div className="text-red-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.redFlags.item9')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.redFlags.item9Desc')}
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -150,6 +234,90 @@ export default function Home() {
               </h3>
               <p className="text-gray-700">
                 {t('home.advice.item3Desc')}
+              </p>
+            </div>
+
+            <div className="bg-green-50 rounded-lg p-6 border border-green-200">
+              <div className="text-green-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.advice.item4')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.advice.item4Desc')}
+              </p>
+            </div>
+
+            <div className="bg-green-50 rounded-lg p-6 border border-green-200">
+              <div className="text-green-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.advice.item5')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.advice.item5Desc')}
+              </p>
+            </div>
+
+            <div className="bg-green-50 rounded-lg p-6 border border-green-200">
+              <div className="text-green-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.advice.item6')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.advice.item6Desc')}
+              </p>
+            </div>
+
+            <div className="bg-green-50 rounded-lg p-6 border border-green-200">
+              <div className="text-green-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.advice.item7')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.advice.item7Desc')}
+              </p>
+            </div>
+
+            <div className="bg-green-50 rounded-lg p-6 border border-green-200">
+              <div className="text-green-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.advice.item8')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.advice.item8Desc')}
+              </p>
+            </div>
+
+            <div className="bg-green-50 rounded-lg p-6 border border-green-200">
+              <div className="text-green-600 mb-4">
+                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                {t('home.advice.item9')}
+              </h3>
+              <p className="text-gray-700">
+                {t('home.advice.item9Desc')}
               </p>
             </div>
           </div>
