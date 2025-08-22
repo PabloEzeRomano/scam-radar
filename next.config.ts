951ef1b8e24c2544d8a69eaefe8f7c653cf5cacd
Pablo Romano <pabloezeromano@gmail.com>
2025-08-22 11:57:00 -0300

    Add base path to update subroute in main url
    Signed-off-by: Pablo Romano <pabloezeromano@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// next.config.mjs
+const isDev = process.env.NODE_ENV !== 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/scam-radar',
+  async redirects() {
+    return isDev
+      ? [
+          {
+            source: '/', // capturá la raíz
+            destination: '/scam-radar',
+            permanent: false,
+            basePath: false, // importante: que NO duplique el basePath
+          },
+        ]
+      : [];
+  },
 };
 
 export default nextConfig;
